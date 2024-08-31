9795a64260209a1bbc72179d983a5012a8aaebe3
zubairahmedkha <zubairahmedniz@gmail.com>
2024-08-31 14:50:26 +0500

    api work

---
+++ api01/api/src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+ 
+export default function Home() {
+  return (<div><header className="flex justify-between 
+  sm:bg-blue-400  md:bg-slate-600  xl:bg-green-400    ">
+    
+    
+     <h1 className=" m-4 h-20 w-20 bg-red-400"> </h1>
+     <h1 className=" m-4 h-20 w-20 bg-red-400"> </h1>
+     <h1 className=" m-4 h-20 w-20 bg-red-400"> </h1>
+     
+     
+     
+     
+     </header>
+    
+  </div>
+    )}
