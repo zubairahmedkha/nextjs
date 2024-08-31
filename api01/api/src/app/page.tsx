c8c64c4e006d0225f6a4270f740793cfe2fab099
zubairahmedkha <zubairahmedniz@gmail.com>
2024-08-31 19:30:37 +0500

    add api

--- api01/api/src/app/page.tsx
+++ api01/api/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
  
 export default function Home() {
   return (<div><header className="flex justify-between 
-  sm:bg-blue-400  md:bg-slate-600  xl:bg-green-400    ">
+  sm:bg-blue-400  md:bg-slate-600  xl:bg-green-500    ">
     
     
      <h1 className=" m-4 h-20 w-20 bg-red-400"> </h1>
@@ -11,7 +11,7 @@ export default function Home() {
      
      
      
-     
+
      
      </header>
     
