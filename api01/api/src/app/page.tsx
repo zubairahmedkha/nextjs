0f9f01ac6e351ac4a6f6bbb7960a530f32e625f6
zubairahmedkha <zubairahmedniz@gmail.com>
2024-08-31 19:52:47 +0500

    Update page

--- api01/api/src/app/page.tsx
+++ api01/api/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
  
 export default function Home() {
   return (<div><header className="flex justify-between 
-  sm:bg-blue-400  md:bg-slate-600  xl:bg-green-500    ">
+  sm:bg-blue-400  md:bg-slate-600  xl:bg-green-400    ">
     
     
      <h1 className=" m-4 h-20 w-20 bg-red-400"> </h1>
